96388eb69a3a506fd7eadf2c05cba22eb6f03c8b
jyore <joey@jyore.com>
2016-08-22 22:54:23 -0500

    removed (current) unsupported methods

--- index.js
+++ index.js
@@ -43,14 +43,10 @@ AWS.DynamoDB.DocumentClient.prototype.configCache = function(config) {
   var enabled = true;
 
   var function_cache = {
-    batchGet:   this.batchGet,
-    batchWrite: this.batchWrite,
-    createSet:  this.createSet,
     delete:     this.delete,
     get:        this.get,
     query:      this.query,
-    scan:       this.scan,
-    update:     this.update
+    scan:       this.scan
   };
 
 
@@ -72,27 +68,6 @@ AWS.DynamoDB.DocumentClient.prototype.configCache = function(config) {
 
   // Method Wrappers
   // ---------------------------------------------
-
-  this.batchGet = (function() {
-    return function() {
-      throw new Error('dynamo-cache: Method Not Implemented');
-      //function_cache.batchGet.apply(this,arguments);
-    }
-  })();
-
-  this.batchWrite = (function() {
-    return function() {
-      throw new Error('dynamo-cache: Method Not Implemented');
-      //function_cache.batchWrite.apply(this,arguments);
-    }
-  })();
-
-  this.createSet = (function() {
-    return function() {
-      throw new Error('dynamo-cache: Method Not Implemented');
-      //function_cache.createSet.apply(this,arguments);
-    }
-  })();
 
   this.delete = (function() {
     return function(params,callback) {
@@ -137,23 +112,64 @@ AWS.DynamoDB.DocumentClient.prototype.configCache = function(config) {
   })();
 
   this.query = (function() {
-    return function() {
-      throw new Error('dynamo-cache: Method Not Implemented');
-      //function_cache.query.apply(this,arguments);
+    return function(params,callback) {
+
+      if(enabled) {
+
+        if(options.debug) {
+          logger(params);
+        }
+
+        var key = util.hash(util.buildKey(params));
+
+        cache.get(key, function(err,reply) {
+
+          if(err || reply) {
+            callback(err,JSON.parse(reply));
+          } else {
+
+            function_cache.get(params, function(err,data) {
+              if(!err) { 
+                cache.put(key,JSON.stringify(data));
+              }
+              callback(err,data);
+            });
+          }
+        });
+      } else {
+        function_cache.query(params,callback);
+      }
     }
   })();
 
   this.scan = (function() {
-    return function() {
-      throw new Error('dynamo-cache: Method Not Implemented');
-      //function_cache.scan.apply(this,arguments);
-    }
-  })();
+    return function(params,callback) {
 
-  this.update = (function() {
-    return function() {
-      throw new Error('dynamo-cache: Method Not Implemented');
-      //function_cache.update.apply(this,arguments);
+      if(enabled) {
+
+        if(options.debug) {
+          logger(params);
+        }
+
+        var key = util.hash(util.buildKey(params));
+
+        cache.get(key, function(err,reply) {
+
+          if(err || reply) {
+            callback(err,JSON.parse(reply));
+          } else {
+
+            function_cache.get(params, function(err,data) {
+              if(!err) { 
+                cache.put(key,JSON.stringify(data));
+              }
+              callback(err,data);
+            });
+          }
+        });
+      } else {
+        function_cache.scan(params,callback);
+      }
     }
   })();
 
